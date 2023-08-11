8dba1016ef5521022f1e64a4f4477933ebef874f
Roy Carter <roycartercpa@gmail.com>
2023-08-11 19:24:39 -0400

    final update on form project with just password verify for js work.

--- java.js
+++ java.js
@@ -1,1 +1,17 @@
-alert("heyman");
+let verify = document.getElementById('password');
+let verify1 = document.getElementById('confirm-password');
+let passOne 
+let passOne1
+
+verify1.addEventListener("blur", (e) => {
+  passOne = verify.value;
+  passOne1 = verify1.value;
+  console.log(passOne);
+  console.log(passOne1);
+  if (passOne != passOne1) {
+    alert("Passwords are not the same. Please enter again")
+    verify1.value = "";
+    verify.value = "";
+  }
+})
+
